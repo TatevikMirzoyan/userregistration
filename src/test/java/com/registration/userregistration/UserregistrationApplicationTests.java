0c5600f6203c23d9d7ef3addc00378edb186e7d9
TatevMT <mirzoyantat@gmail.com>
2020-12-03 02:53:50 +0400

    User registration program with Spring MVC CRUD

---
+++ src/test/java/com/registration/userregistration/UserregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.userregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserregistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
